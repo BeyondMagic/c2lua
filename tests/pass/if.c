e38ad55f4f4a701b4662ecf06849ecdde18072d8
Liviarodrigues1 <liviarodriguesre@gmail.com>
2025-11-27 00:01:08 -0300

    feat(optimizer): eliminar if com condição constante

---
+++ tests/pass/if.c
@@ -0,0 +1,18 @@
+int main()
+{
+	int a = 1;
+	if (a == 1)
+	{
+		printf("numero: %d\n", a);
+	}
+	if (1)
+	{
+		printf("verdadeiro: %d\n", a);
+	}
+	if (0)
+	{
+		printf("falso: %d\n", a);
+	}
+	puts("fim");
+	return 0;
+}
